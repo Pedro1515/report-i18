05bdfa6cb82a670d7a1320dc2c2736d4362788fe
pedro <pedro.moran@crowdaronline.com>
2021-03-26 11:41:13 -0300

    cambio x del boton close a font-medium

--- src/pages/projects/[id]/runs/[rid]/index.tsx
+++ src/pages/projects/[id]/runs/[rid]/index.tsx
@@ -831,7 +831,7 @@ function Dropdown({run, runs}) {
           <div style={{height: activedStyle ? "80vh" : "auto"}} className={`absolute left-0 mt-2 w-56 origin-top-right`}>
             <nav style={{height: "70%"}} className="rounded-md border">
             <div style={{right: "-38px"}} className="inline-block bg-white absolute border py-1 px-2 shadow-sm rounded-md cursor-pointer transition duration-200 hover:bg-gray-100">
-              <span className="leading-none text-xl font-extrabold" aria-hidden="true">&times;</span>
+              <span className="leading-none text-xl font-medium" aria-hidden="true">&times;</span>
             </div>
               <ul className="h-full overflow-y-overlay rounded-md bg-white">
                 {runs?.content.map(run => {
